e1e5399ffe79907d62e6c59fd253bfd86907b4f6
Brandon Woodthorpe <36206121+Marmaduke45@users.noreply.github.com>
2022-07-06 14:03:40 -0400

    fixed assets not loading

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 const enemyData = {
     orc: {
         name: "Orc",
-        avatar: "images/orc.png",
+        avatar: "./images/orc.png",
         health: 30,
         diceCount: 2,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "images/demon.png",
+        avatar: "./images/demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "images/goblin.png",
+        avatar: "./images/goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
@@ -25,21 +25,21 @@ const enemyData = {
 const heroData = {
     wizard: {
         name: "Wizard",
-        avatar: "images/wizard.png",
+        avatar: "./images/wizard.png",
         health: 65,
         diceCount: 3,
         currentDiceScore: []
     },
     brute: {
         name: "Brute",
-        avatar: "images/brute.png",
+        avatar: "./images/brute.png",
         health: 80,
         diceCount: 2,
         currentDiceScore: []
     },
     vampire: {
         name: "Vampire",
-        avatar: "images/vampire.png",
+        avatar: "./images/vampire.png",
         health: 60,
         diceCount: 1,
         currentDiceScore: []
@@ -47,7 +47,7 @@ const heroData = {
     },
     gambler: {
         name: "Gambler",
-        avatar: "images/gambler.png",
+        avatar: "./images/gambler.png",
         health: 40,
         diceCount: 5,
         currentDiceScore: []
